b8d1237b0450ac17830fe2d6167fb5f97edae021
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2023-12-18 23:15:55 -0300

    changed ci

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -13,48 +13,43 @@ allprojects {
     version = "0.0.2"
 }
 
-task("configureSigning") {
-    dependsOn("importSecrets")
-    doLast {
-        nexusPublishing {
+nexusPublishing {
 
-            val localProperties = Properties()
-            val localPropertiesFile = rootProject.file("local.properties")
+    importSecrets()
+    val localProperties = Properties()
+    val localPropertiesFile = project.rootProject.file("local.properties")
 
-            // Configure maven central repository
-            // https://github.com/gradle-nexus/publish-plugin#publishing-to-maven-central-via-sonatype-ossrh
-            repositories {
-                sonatype { // only for users registered in Sonatype after 24 Feb 2021
-                    nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-                    snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-                    if (localPropertiesFile.exists()) {
-                        localPropertiesFile.inputStream().use {
-                            localProperties.load(it)
-                            this.username.set(localProperties.getProperty("ossrhUsername"))
-                            this.password.set(localProperties.getProperty("ossrhPassword"))
-                        }
-                    }
-
-
+    // Configure maven central repository
+    // https://github.com/gradle-nexus/publish-plugin#publishing-to-maven-central-via-sonatype-ossrh
+    repositories {
+        sonatype { // only for users registered in Sonatype after 24 Feb 2021
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            if (localPropertiesFile.exists()) {
+                localPropertiesFile.inputStream().use {
+                    localProperties.load(it)
+                    this.username.set(localProperties.getProperty("ossrhUsername"))
+                    this.password.set(localProperties.getProperty("ossrhPassword"))
                 }
             }
+
+
         }
     }
 }
-task("importSecrets") {
-    doLast {
-        val secretPropsFile = project.rootProject.file("local.properties")
-        if (secretPropsFile.exists()) {
-            secretPropsFile.reader().use {
-                Properties().apply {
-                    load(it)
-                }
-            }.onEach { (name, value) ->
-                ext[name.toString()] = value
+fun importSecrets() {
+
+    val secretPropsFile = project.rootProject.file("local.properties")
+    if (secretPropsFile.exists()) {
+        secretPropsFile.reader().use {
+            Properties().apply {
+                load(it)
             }
-        } else {
-            ext["ossrhUsername"] = System.getenv("OSSRH_USERNAME")
-            ext["ossrhPassword"] = System.getenv("OSSRH_PASSWORD")
+        }.onEach { (name, value) ->
+            ext[name.toString()] = value
         }
+    } else {
+        ext["ossrhUsername"] = System.getenv("OSSRH_USERNAME")
+        ext["ossrhPassword"] = System.getenv("OSSRH_PASSWORD")
     }
 }
